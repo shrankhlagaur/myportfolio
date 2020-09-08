a8c18bdfa1252481a032358722ec7d4385b785b3
shrankhlagaur <shrankhlagaur09@gmail.com>
2020-09-08 22:26:25 +0530

    second push

--- javascript/home1.js
+++ javascript/home1.js
@@ -1,14 +1,14 @@
-//$().ready(function () {
-//    "use strict";
-//		// set up hover panels
-//		// although this can be done without JavaScript, we've attached these events
-//		// because it causes the hover to be triggered when the element is tapped on a touch device
-//    $('.hover').hover(function () {
-//        $(this).addClass('flip');
-//    }, function () {
-//			$(this).removeClass('flip');
-//		});
-//	});
+$().ready(function () {
+    "use strict";
+		// set up hover panels
+		// although this can be done without JavaScript, we've attached these events
+		// because it causes the hover to be triggered when the element is tapped on a touch device
+    $('.hover').hover(function () {
+        $(this).addClass('flip');
+    }, function () {
+			$(this).removeClass('flip');
+		});
+	});
 
 
 
